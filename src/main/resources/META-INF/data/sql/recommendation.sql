1cb86a275f3f3e683a59a5d378532342c6310c20
julesbond007 <jay.paulynice@gmail.com>
2015-07-01 22:15:03 -0400

    clean up

--- src/main/resources/META-INF/data/sql/recommendation.sql
+++ src/main/resources/META-INF/data/sql/recommendation.sql
@@ -1,12 +1,11 @@
---populate users
+/** populate users table */
 insert into users (name,email) values ('jay','jay.paulynice@gmail.com');
 insert into users (name,email) values ('nate','nate.paulynice@gmail.com');
 insert into users (name,email) values ('aiden','aiden.paulynice@gmail.com');
 insert into users (name,email) values ('mella','mella.paulynice@gmail.com');
 insert into users (name,email) values ('me','me.paulynice@gmail.com');
 
-
---populate items
+/** populate movies table */
 insert into items (name,type,imdb_id) values('captain america','ACTION','tt0103923');
 insert into items (name,type,imdb_id) values('iron man','ACTION','tt0371746');
 insert into items (name,type,imdb_id) values('rambo','ACTION','tt0462499');
@@ -14,8 +13,7 @@ insert into items (name,type,imdb_id) values('mcgyver','ACTION','tt0110419');
 insert into items (name,type,imdb_id) values('cars','FAMILY','tt0317219');
 insert into items (name,type,imdb_id) values('planes','FAMILY','tt1691917');
 
-
---populate user preferences
+/** populate taste_preferences table */
 insert into taste_preferences (user_id,item_id,preference) values (1,1,1);
 insert into taste_preferences (user_id,item_id,preference) values (2,2,0);
 insert into taste_preferences (user_id,item_id,preference) values (3,3,0);
@@ -29,8 +27,7 @@ insert into taste_preferences (user_id,item_id,preference) values (5,3,1);
 insert into taste_preferences (user_id,item_id,preference) values (1,3,1);
 insert into taste_preferences (user_id,item_id,preference) values (1,6,1);
 
-
---populate item similarity
+/** populate taste_item_similarity table */
 insert into taste_item_similarity (item_id_a,item_id_b,similarity) values (1,2,1);
 insert into taste_item_similarity (item_id_a,item_id_b,similarity) values (1,3,1);
 insert into taste_item_similarity (item_id_a,item_id_b,similarity) values (1,4,0);
