b0239bc77c8fa842db5ede28bcd8b3296352ba90
julesbond007 <jay.paulynice@gmail.com>
2016-02-27 16:56:31 -0500

    tests

--- src/test/java/com/recommendation/rest/provider/AppExceptionMapperTest.java
+++ src/test/java/com/recommendation/rest/provider/AppExceptionMapperTest.java
@@ -1,7 +1,9 @@
 package com.recommendation.rest.provider;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 import com.recommendation.config.SpringConfig;
+import com.recommendation.exception.UserNotFoundException;
 
 @ContextConfiguration(classes = SpringConfig.class)
 public class AppExceptionMapperTest extends AbstractTestNGSpringContextTests {
@@ -25,5 +28,21 @@ public class AppExceptionMapperTest extends AbstractTestNGSpringContextTests {
     public void test500() {
         final Response res = mapper.toResponse(new RuntimeException());
         assertNotNull(res);
+        assertEquals(res.getStatus(), 500);
+    }
+
+    @Test
+    public void test404() {
+        final Response res = mapper.toResponse(new UserNotFoundException(
+                "no user found with id: 9899"));
+        assertNotNull(res);
+        assertEquals(res.getStatus(), 404);
+    }
+
+    @Test
+    public void testWebappException() {
+        final Response res = mapper.toResponse(new WebApplicationException());
+        assertNotNull(res);
+        assertEquals(res.getStatus(), 500);
     }
 }
